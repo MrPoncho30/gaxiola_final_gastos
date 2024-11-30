fca48affa18b486eef7e98c4c94b035d4ec26d38
Torres-Alan <adtorresfuentes14@gmail.com>
2024-11-30 17:53:28 -0600

    firbase_addexpenses_commit

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gaxiola_final_gastos/screens/add_expense/blocs/create_categorybloc/create_category_bloc.dart';
+import 'package:gaxiola_final_gastos/screens/add_expense/blocs/create_expense_bloc/create_expense_bloc.dart';
 import 'package:gaxiola_final_gastos/screens/add_expense/blocs/get_categories_bloc/get_categories_bloc.dart';
 import 'package:gaxiola_final_gastos/screens/add_expense/views/add_expense.dart';
 import 'package:gaxiola_final_gastos/screens/home/views/main_screen.dart';
@@ -62,7 +63,12 @@ class _HomeScreenState extends State<HomeScreen> {
                                 CreateCategoryBloc(FirebaseExpenseRepo()),
                           ),
                           BlocProvider(
-                            create: (context) => GetCategoriesBloc(FirebaseExpenseRepo())..add(GetCategories()),
+                            create: (context) => GetCategoriesBloc(FirebaseExpenseRepo())..add(GetCategories()
+                            ),
+                          ),
+                          BlocProvider(
+                            create: (context) =>
+                                CreateExpenseBloc(FirebaseExpenseRepo()),
                           ),
                         ],
                         child: const AddExpense(),
